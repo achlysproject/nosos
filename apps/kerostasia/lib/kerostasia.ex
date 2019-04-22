72ef29d7e0d75bb14aa0b46c16d62ed68cf33758
Igor Kopestenski <igor.kopestenski@uclouvain.be>
2019-04-23 00:23:52 +0200

    examples

---
+++ apps/kerostasia/lib/kerostasia.ex
@@ -0,0 +1,18 @@
+defmodule Kerostasia do
+  @moduledoc """
+  Documentation for Kerostasia.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kerostasia.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/kerostasia/test/kerostasia_test.exs
@@ -0,0 +1,8 @@
+defmodule KerostasiaTest do
+  use ExUnit.Case
+  doctest Kerostasia
+
+  test "greets the world" do
+    assert Kerostasia.hello() == :world
+  end
+end
